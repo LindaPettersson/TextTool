c537fa16fcc8dba2c2f05ee0883bdc0d3a1b76b9
Linda Pettersson <pettersson.linda08@gmail.com>
2018-05-21 12:04:53 +0200

    added angular min files and new directories for index

--- TextToolApp/WebContent/client/js/app.js
+++ TextToolApp/WebContent/client/js/app.js
@@ -1,13 +1,11 @@
 var myApp = angular.module('myApp', ['ngRoute', 'ngTable']).
 config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
-	$routeProvider.when('/home', {templateUrl: 'client/partials/home.html', controller: 'homeController'});
-	$routeProvider.when('/login', {templateUrl: 'client/partials/login.html', controller: 'loginController'});
-	$routeProvider.when('/choosefiles', {templateUrl: 'client/partials/choosefiles.html', controller: 'choosefilesController'});
-	$routeProvider.when('/summarytypes', {templateUrl: 'client/partials/summarytypes.html', controller: 'summarytypesController'});
+	$routeProvider.when('/home', {templateUrl: '/client/partials/home.html', controller: 'homeController'});
+	$routeProvider.when('/login', {templateUrl: '/client/partials/login.html', controller: 'loginController'});
+	$routeProvider.when('/choosefiles', {templateUrl: '/client/partials/choosefiles.html', controller: 'choosefilesController'});
+	$routeProvider.when('/summarytypes', {templateUrl: '/client/partials/summarytypes.html', controller: 'summarytypesController'});
 	$routeProvider.otherwise({redirectTo: '/home'});
 	
-	//remove hashtag from url
-	//$locationProvider.html5Mode({enabled: true, requireBase: false});
 	
 	
 }]);
